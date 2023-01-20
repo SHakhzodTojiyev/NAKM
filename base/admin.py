624e6d8b53ed41300f7c174e59bfe751cd4c1547
SHakhzodTojiyev <tojiyevshakhzod@gmail.com>
2023-01-20 20:40:40 +0500

    done

---
+++ base/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import (Aloqa, Asosiy_Rasimlar, Biz_haqimizda, Elonlar, Farmonlar, Qonunlar, Rahbariyat, Xodimlar, Tadbirlar)
+# Register your models here.
+
+ 
+admin.site.register(Rahbariyat)
+admin.site.register(Xodimlar)
+admin.site.register(Tadbirlar)
+admin.site.register(Elonlar)
+admin.site.register(Qonunlar)
+admin.site.register(Farmonlar)
+admin.site.register(Asosiy_Rasimlar)
+admin.site.register(Aloqa)
+admin.site.register(Biz_haqimizda)
---
+++ base/views.py
@@ -0,0 +1,72 @@
+from django.shortcuts import render
+from .models import (Aloqa, Asosiy_Rasimlar, Biz_haqimizda, Farmonlar, Rahbariyat, Xodimlar, Tadbirlar, Elonlar, Qonunlar)
+
+# Create your views here.
+def home(request):
+  asosiy_rasimlar = Asosiy_Rasimlar.objects.all()
+  biz_haqimizda = Biz_haqimizda.objects.all()
+  tadbir = Tadbirlar.objects.order_by('-id')[:3]
+  aloqa = Aloqa.objects.all()
+  context = {
+    'asosiy_rasimlar': asosiy_rasimlar, 
+    'biz': biz_haqimizda,
+    'tadbir': tadbir,
+    'aloqa': aloqa
+  }
+  return render(request, 'pages/home.html', context)
+
+def about(request):
+  biz_haqimizda = Biz_haqimizda.objects.all()
+  context = {'biz': biz_haqimizda}
+  return render(request, 'pages/about.html', context)
+
+
+def rahbariyat(request):
+  rahbar = Rahbariyat.objects.all()
+  context = {'rahbar' : rahbar}
+  return render(request, 'pages/rahbariyat.html', context)
+
+
+def xodimlar(request):
+  xodim = Xodimlar.objects.all()
+  context = {'xodim' : xodim}
+  return render(request, 'pages/xodimlar.html', context)
+
+
+def tadbirlar(request):
+    tadbirlar = Tadbirlar.objects.all()
+    context = {'tadbir': tadbirlar}
+    return render(request, 'pages/tadbirlar.html', context)
+
+def tadbir(request, pk):
+    tadbir_haqida = Tadbirlar.objects.get(pk = pk)
+    context = {'tadbir_haqida': tadbir_haqida}
+    return render(request, 'pages/tadbir.html', context)
+
+
+def elonlar(request):
+  elonlar = Elonlar.objects.all()
+  context = {'elon': elonlar}
+  return render(request, 'pages/elonlar.html', context)
+
+def elon(request, pk):
+  elon_haqida = Elonlar.objects.get(pk = pk)
+  context = {'elon_haqida': elon_haqida}
+  return render(request, 'pages/elon.html', context)
+
+
+def qonunlar(request):
+  qonun = Qonunlar.objects.order_by("-id").all()
+  context = {'qonun': qonun}
+  return render(request, 'pages/qonunlar.html', context)
+
+def farmonlar(request):
+  farmon = Farmonlar.objects.order_by("-id").all()
+  context = {'farmon': farmon}
+  return render(request, 'pages/farmonlar.html', context)
+
+
+def aloqa(request):
+  aloqa = Aloqa.objects.all()
+  context = {'aloqa': aloqa}
+  return render(request, 'pages/contact.html', context)
