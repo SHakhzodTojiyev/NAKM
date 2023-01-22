5cf5982c4635f55cdf90d82f57641e6b94a9f2ba
SHakhzodTojiyev <tojiyevshakhzod@gmail.com>
2023-01-22 06:11:11 +0500

    done

--- config/settings.py
+++ config/settings.py
@@ -119,14 +119,11 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    # '/var/www/static/',
-]
-
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+STATICFILES_DIRS = [BASE_DIR/"static"]
+
+
+MEDIA_ROOT = BASE_DIR/"media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
